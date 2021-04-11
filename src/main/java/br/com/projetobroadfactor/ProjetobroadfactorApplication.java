b9439058399381ad8e58532d0aaf45cc65dbb288
Wagner Martins <wagnermartinssantana@gmail.com>
2021-04-11 01:11:07 -0300

    Resolvendo Bug de pastas HTML

---
+++ src/main/java/br/com/projetobroadfactor/ProjetobroadfactorApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetobroadfactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobroadfactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobroadfactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetobroadfactor/ProjetobroadfactorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetobroadfactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobroadfactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
